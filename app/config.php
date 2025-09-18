d0829adb6df82d9ada0278dc2a38f96bfe05428c
Indiafollow <MakeFollowerGo@gmail.com>
2025-09-18 06:10:31 +0530

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,8 +1,7 @@
 <?php
-
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', 'yourpass');
-define('DB_NAME', 'smm_free');
-define('TIMEZONE', 'Asia/Ho_Chi_Minh');
-define('ENCRYPTION_KEY', '202dd507882ef55dbbc23f7e84dcfc8d');
+define('DB_HOST', 'sql105.infinityfree.com');
+define('DB_USER', 'if0_39859889');
+define('DB_PASS', '1L50Ad7WpJn');
+define('DB_NAME', 'if0_39859889_1');   // List of MySQL Databases me jo exact naam dikhta hai
+define('TIMEZONE', 'Asia/Kolkata');
+define('ENCRYPTION_KEY', '202d0507882ef55ddbc23f7e84dcfe8d');
