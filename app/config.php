c9662975b919afd9d9d5d9a06fb3c0afdb535668
Indiafollow <MakeFollowerGo@gmail.com>
2025-09-18 08:18:07 +0530

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', '');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_HOST', 'db.fr-pari1.bengt.wasmernet.com');
+define('DB_USER', 'b70da2fb7d4280007fd4c2402d6e');
+define('DB_PASS', '068cb70d-a2fb-7f65-8000-7659396e6ed5');
+define('DB_NAME', 'dbW4mwkgSUhmN6CMFRe8Zm9E');
 
 define('TIMEZONE', 'Asia/Kolkata');
 define('ENCRYPTION_KEY', '202d0507882ef55ddbc23f7e84dcfe8d');
