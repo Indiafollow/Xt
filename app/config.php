8398faa08a61eafe29a918b375704873f2ce3211
Indiafollow <MakeFollowerGo@gmail.com>
2025-09-18 07:28:57 +0530

    Update config.php

--- app/config.php
+++ app/config.php
@@ -2,6 +2,7 @@
 define('DB_HOST', 'db.fr-pari1.bengt.wasmernet.com');
 define('DB_USER', 'b585b741795880005f1cfc1f8780');
 define('DB_PASS', '068cb585-b741-7b05-8000-3453b1959268');
-define('DB_NAME', 'smm_free');   // List of MySQL Databases me jo exact naam dikhta hai
+define('DB_NAME', 'smm_free');
+
 define('TIMEZONE', 'Asia/Kolkata');
 define('ENCRYPTION_KEY', '202d0507882ef55ddbc23f7e84dcfe8d');
