ee6ce08ff561081097c30bcf1780f67e8c9746fa
Indiafollow <MakeFollowerGo@gmail.com>
2025-09-18 08:06:09 +0530

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,65 +1,28 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+$active_group='default'; $query_builder=TRUE;
 
-$active_group  = 'default';
-$query_builder = TRUE;
-
-$db['default'] = array(
+$db['default']=array(
   'dsn'      => '',
   'hostname' => DB_HOST,
   'username' => DB_USER,
   'password' => DB_PASS,
   'database' => DB_NAME,
   'dbdriver' => 'mysqli',
-  'port'     => 10272,          // Wasmer port
+  'port'     => ,
   'pconnect' => FALSE,
-  'db_debug' => (ENVIRONMENT !== 'production'),
+  'db_debug' => (ENVIRONMENT!=='production'),
   'cache_on' => FALSE,
   'cachedir' => '',
   'char_set' => 'utf8mb4',
-  'dbcollat' => 'utf8mb4_general_ci',
+  'dbcollat' => 'utf8mb4_unicode_ci',
   'swap_pre' => '',
-  'encrypt'  => FALSE,          // TRUE only if Wasmer requires TLS + CA
+  'encrypt'  => FALSE,
   'compress' => FALSE,
   'stricton' => FALSE,
   'failover' => array(),
   'save_queries' => TRUE,
   'options'  => array(
-      MYSQLI_OPT_CONNECT_TIMEOUT => 8
+      MYSQLI_OPT_CONNECT_TIMEOUT => 10
   )
-);
-| 				When you run a query, with this setting set to TRUE (default),
-| 				CodeIgniter will store the SQL statement for debugging purposes.
-| 				However, this may cause high memory usage, especially if you run
-| 				a lot of SQL queries ... disable this to avoid that problem.
-|
-| The $active_group variable lets you choose which connection group to
-| make active.  By default there is only one group (the 'default' group).
-|
-| The $query_builder variables lets you determine whether or not to load
-| the query builder class.
-*/
-$active_group = 'default';
-$query_builder = TRUE;
-
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => DB_HOST,
-	'username' => DB_USER,
-	'password' => DB_PASS,
-	'database' => DB_NAME,
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8mb4',
-	'dbcollat' => 'utf8mb4_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
 );
